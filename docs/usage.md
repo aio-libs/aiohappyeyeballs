2e25a98f2339d84bc7951ad17f0b38c104a97a71
J. Nick Koston <nick@koston.org>
2023-12-10 18:21:11 -1000

    feat: add addr_to_addr_info util for converting addr to addr_infos (#27)

--- docs/usage.md
+++ docs/usage.md
@@ -22,4 +22,7 @@ aiohappyeyeballs.pop_addr_infos_interleave(addr_info, 1)
 
 # Remove all matching address from addr_info
 aiohappyeyeballs.remove_addr_infos(addr_info, "dead::beef::")
+
+# Convert a local_addr to local_addr_infos
+local_addr_infos = addr_to_addr_infos(("127.0.0.1",0))
 ```
--- src/aiohappyeyeballs/__init__.py
+++ src/aiohappyeyeballs/__init__.py
@@ -2,11 +2,12 @@ __version__ = "2.0.0"
 
 from .impl import start_connection
 from .types import AddrInfoType
-from .utils import pop_addr_infos_interleave, remove_addr_infos
+from .utils import addr_to_addr_infos, pop_addr_infos_interleave, remove_addr_infos
 
 __all__ = (
     "start_connection",
     "AddrInfoType",
     "remove_addr_infos",
     "pop_addr_infos_interleave",
+    "addr_to_addr_infos",
 )
