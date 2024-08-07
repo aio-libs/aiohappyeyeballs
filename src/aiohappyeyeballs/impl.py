7bbb2bf0feb3994953a52a1d606e682acad49cb8
J. Nick Koston <nick@koston.org>
2024-08-07 07:51:55 -0500

    fix: preserve errno if all exceptions have the same errno (#77)

--- src/aiohappyeyeballs/impl.py
+++ src/aiohappyeyeballs/impl.py
@@ -73,7 +73,7 @@ async def start_connection(
         addr_infos = _interleave_addrinfos(addr_infos, interleave)
 
     sock: Optional[socket.socket] = None
-    exceptions: List[List[Exception]] = []
+    exceptions: List[List[OSError]] = []
     if happy_eyeballs_delay is None or single_addr_info:
         # not using happy eyeballs
         for addrinfo in addr_infos:
@@ -99,20 +99,28 @@ async def start_connection(
     if sock is None:
         all_exceptions = [exc for sub in exceptions for exc in sub]
         try:
+            first_exception = all_exceptions[0]
             if len(all_exceptions) == 1:
-                raise all_exceptions[0]
+                raise first_exception
             else:
                 # If they all have the same str(), raise one.
-                model = str(all_exceptions[0])
+                model = str(first_exception)
                 if all(str(exc) == model for exc in all_exceptions):
-                    raise all_exceptions[0]
+                    raise first_exception
                 # Raise a combined exception so the user can see all
                 # the various error messages.
-                raise OSError(
-                    "Multiple exceptions: {}".format(
-                        ", ".join(str(exc) for exc in all_exceptions)
-                    )
+                msg = "Multiple exceptions: {}".format(
+                    ", ".join(str(exc) for exc in all_exceptions)
                 )
+                # If the errno is the same for all exceptions, raise
+                # an OSError with that errno.
+                first_errno = first_exception.errno
+                if all(
+                    isinstance(exc, OSError) and exc.errno == first_errno
+                    for exc in all_exceptions
+                ):
+                    raise OSError(first_errno, msg)
+                raise OSError(msg)
         finally:
             all_exceptions = None  # type: ignore[assignment]
             exceptions = None  # type: ignore[assignment]
@@ -122,12 +130,12 @@ async def start_connection(
 
 async def _connect_sock(
     loop: asyncio.AbstractEventLoop,
-    exceptions: List[List[Exception]],
+    exceptions: List[List[OSError]],
     addr_info: AddrInfoType,
     local_addr_infos: Optional[Sequence[AddrInfoType]] = None,
 ) -> socket.socket:
     """Create, bind and connect one socket."""
-    my_exceptions: list[Exception] = []
+    my_exceptions: list[OSError] = []
     exceptions.append(my_exceptions)
     family, type_, proto, _, address = addr_info
     sock = None
