db8254cc3f362474f4ea4f5915e2b09c02b7a01b
J. Nick Koston <nick@koston.org>
2025-03-11 15:20:07 -1000

    fix: use typing instead of collections.abc for Callable

--- tests/test_staggered.py
+++ tests/test_staggered.py
@@ -94,6 +94,8 @@ def test_callable_import_from_typing():
     Test that Callable is imported from typing.
 
     PY3.9: https://github.com/python/cpython/issues/87131
+
+    Drop this test when we drop support for Python 3.9.
     """
     assert Callable is CallableTyping
     assert Callable is not CallableABC
